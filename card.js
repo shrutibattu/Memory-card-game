9436b71b7f95aed143bbace265bb3577fa0618ec
Shruti Battu <shrutibattu@MacBookPro-16inch.local>
2021-05-26 19:19:01 +0100

    Changed the entire styles and code

--- card.js
+++ card.js
@@ -1,122 +1,44 @@
-let cards = document.querySelectorAll(".card_inner");
-let firstClick = false;
-let cardPair = [];
-
-cards.forEach((card) => {
-  card.state = "unclicked";
-});
-
-shuffle();
-
-for (let i = 0; i < cards.length; i++) {
-  cards[i].addEventListener("click", () => {});
-  if (!firstClick) {
-    time();
+const cards = document.querySelectorAll(".memory-card");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+function flipCard() {
+  if (lockBoard) return;
+  this.classList.add("flip");
+  if (!hasFlippedCard) {
+    //firstclick
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
   }
-  firstClick = true;
-  if (cards[i].state == "unclicked") {
-    cards[i].style.transform = "rotateY(180deg)";
-    cards[i].state = "clicked";
-    counter++;
-    cardPair.push(cards[i]);
-    check();
-  } else if (cards[i].state == "clicked") {
-    cards[i].style.transform = "rotateY(0deg)";
-    cards[i].state = "unclicked";
-    counter--;
-    cardPair = [];
-  }
+  //secondclick
+  hasFlippedCard = false;
+  secondCard = this;
+
+  checkForMatch();
 }
-function check() {
-  if (cardPair.length == 2) {
-    if (
-      cardPair[0].querySelector("img").src == cardPair[1].querySelector("img")
-    ) {
-      matched();
-    } else {
-      unmatched(cardPair[0], cardPair[1]);
-    }
+function checkForMatch() {
+  //do cards match
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    //its a match
+    disableCards();
+  } else {
+    //not a match
+    unflipCards();
   }
 }
-
-function matched() {
-  cardPair[0].state = "blocked";
-  cardPair[1].state = "blocked";
-  counter = 0;
-  cardPair = [];
-  let score = document.querySelector("#score").innerHTML;
-  score++;
-  document.querySelector("#score").innerHTML = score;
+function disableCards() {
+  //its a match
+  firstCard.removeEventListner("click", flipCard);
+  secondCard.removeEventListner("click", flipCard);
 }
-
-function unmatched(x, y) {
+function unflipCards() {
+  lockBoard = true;
+  //not a match
   setTimeout(() => {
-    x.state = "unclicked";
-    y.state = "unclicked";
-    x.style.transform = "rotateY(0deg)";
-    y.style.transform = "rotateY(0deg)";
-  }, 750);
-  cardPair[0].state = "blocked";
-  cardPair[1].state = "blocked";
-  counter = 0;
-  cardPair = [];
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    lockBoard = false;
+  }, 1500);
 }
-
-// function unmatched(x, y) {
-//   setTimeout(() => {
-//     // cardPair[0]
-//     x.style.transform = "rotateY(0deg)";
-//     // cardPair[1]
-//     y.style.transform = "rotateY(0deg)";
-//   }, 750);
-//   cardPair[0].state = "unclicked";
-//   cardPair[1].state = "unclicked";
-//   counter = 0;
-//   cardPair = [];
-// }
-function time() {
-  let secs = 0;
-  let mins = 0;
-  let SS;
-  let MM;
-
-  setInterval(() => {
-    secs++;
-    if (secs == 60) {
-      secs = 0;
-      mins++;
-    }
-    secs < 10 ? (SS = `0${secs}`) : (SS = `${secs}`);
-    mins < 10 ? (MM = `0${mins}`) : (SS = `${mins}`);
-    document.querySelector("#time").innerHTML = `${MM}:${SS}`;
-  }, 1000);
-}
-function shuffle() {
-  let images = document.querySelectorAll("img");
-  let srcs = [
-    "images/berries.jpg",
-    "images/cardmom.jpg",
-    "images/chilli.jpg",
-    "images/citrus.jpg",
-    "images/citrusjuice.jpg",
-    "images/lavender.jpg",
-    "images/mint.jpg",
-    "images/spice.jpg",
-    "images/berries.jpg",
-    "images/cardmom.jpg",
-    "images/chilli.jpg",
-    "images/citrus.jpg",
-    "images/citrusjuice.jpg",
-    "images/lavender.jpg",
-    "images/mint.jpg",
-    "images/spice.jpg",
-  ];
-
-  for (let i = srcs.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * i);
-    let temp = srcs[i];
-    srcs[i] = srcs[j];
-    srcs[j] = temp;
-  }
-  for (let i = 0; i < images.length; i++) images[i].src = srcs[i];
-}
+cards.forEach((card) => card.addEventListener("click", flipCard));
